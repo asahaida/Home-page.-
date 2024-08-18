84ff3711974c6d9a51f2593d33d176de1412bd0d
asahaida <156024925+asahaida@users.noreply.github.com>
2024-08-17 21:20:33 -0500

    finally fix - last cose I looked at -- at least it's all cleaned up now...

--- js/index.js
+++ js/index.js
@@ -1,24 +1,42 @@
 function fade() {
-$('.preloader').fadeOut("slow");
-}
-setTimeout(fade, 3000);
-
-$(".searchButton").on("click", function(){
-  $(".searchButton").toggleClass("active");
-  if ($(".searchButton").hasClass("active")) {
-    $(".searchBar").css("height", "100vh");
-    $("#searchForm").css("opacity","1");
-    $(".searchIcon").removeClass("fa-search").addClass("fa-times");
+  $('.preloader').fadeOut("slow");
   }
-  else {
-    $(".searchBar").css('height', "0vh");
-    $("#searchForm").css("opacity","0");
-    $(".searchIcon").removeClass("fa-times").addClass("fa-search");
+  setTimeout(fade, 3000);
+  
+  $(".searchButton").on("click", function(){
+    $(".searchButton").toggleClass("active");
+    if ($(".searchButton").hasClass("active")) {
+      $(".searchBar").css("height", "100vh");
+      $("#searchForm").css("opacity","1");
+      $(".searchIcon").removeClass("fa-search").addClass("fa-times");
+    }
+    else {
+      $(".searchBar").css('height', "0vh");
+      $("#searchForm").css("opacity","0");
+      $(".searchIcon").removeClass("fa-times").addClass("fa-search");
+    }
+  });
+  
+  $(".toggle").click(function(){
+    $(this).find(".toggleContainer").toggleClass("active");
+    $(this).siblings(".toggle").find(".toggleContainer").removeClass("active");
+  });
+   
+  var acc = document.getElementsByClassName("accordion");
+  
+  var i;
+  
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+  
+      this.classList.toggle("active");
+  
+  
+      var panel = this.nextElementSibling;
+      if (panel.style.display === "block") {
+        panel.style.display = "none";
+      } else {
+        panel.style.display = "block";
+      }
+    });
   }
-});
-
-$(".toggle").click(function(){
-  $(this).find(".toggleContainer").toggleClass("active");
-  $(this).siblings(".toggle").find(".toggleContainer").removeClass("active");
-});
- 
